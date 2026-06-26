57e3c6a64639d25bbd70a38173677e6c8b20282c
KonTy <9524513+KonTy@users.noreply.github.com>
2026-06-26 00:10:33 -0700

    WIP: save current local SQL changes

--- sql/custom/world/2021_10_02_world_trainer_extend.sql
+++ sql/custom/world/2021_10_02_world_trainer_extend.sql
@@ -1,5 +1,22 @@
-ALTER TABLE `trainer` ADD COLUMN `raceMask` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `trainer` ADD COLUMN `classMask` INT(10) UNSIGNED NOT NULL DEFAULT '0';
+SET @trainer_has_table := (SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'trainer');
+SET @trainer_spell_has_table := (SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'trainer_spell');
 
-ALTER TABLE `trainer_spell` ADD COLUMN `raceMask` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `trainer_spell` ADD COLUMN `classMask` INT(10) UNSIGNED NOT NULL DEFAULT '0';
+SET @alter_trainer_1 := IF(@trainer_has_table > 0, 'ALTER TABLE `trainer` ADD COLUMN `raceMask` INT(10) UNSIGNED NOT NULL DEFAULT "0"', 'SELECT 1');
+PREPARE stmt1 FROM @alter_trainer_1;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+
+SET @alter_trainer_2 := IF(@trainer_has_table > 0, 'ALTER TABLE `trainer` ADD COLUMN `classMask` INT(10) UNSIGNED NOT NULL DEFAULT "0"', 'SELECT 1');
+PREPARE stmt2 FROM @alter_trainer_2;
+EXECUTE stmt2;
+DEALLOCATE PREPARE stmt2;
+
+SET @alter_trainer_spell_1 := IF(@trainer_spell_has_table > 0, 'ALTER TABLE `trainer_spell` ADD COLUMN `raceMask` INT(10) UNSIGNED NOT NULL DEFAULT "0"', 'SELECT 1');
+PREPARE stmt3 FROM @alter_trainer_spell_1;
+EXECUTE stmt3;
+DEALLOCATE PREPARE stmt3;
+
+SET @alter_trainer_spell_2 := IF(@trainer_spell_has_table > 0, 'ALTER TABLE `trainer_spell` ADD COLUMN `classMask` INT(10) UNSIGNED NOT NULL DEFAULT "0"', 'SELECT 1');
+PREPARE stmt4 FROM @alter_trainer_spell_2;
+EXECUTE stmt4;
+DEALLOCATE PREPARE stmt4;
--- sql/updates/world/3.3.5/2024_08_17_00_world.sql
+++ sql/updates/world/3.3.5/2024_08_17_00_world.sql
@@ -1,3 +1,16 @@
 -- TDB 335.24081 world
-UPDATE `version` SET `db_version`='TDB 335.24081', `cache_id`=24081 LIMIT 1;
+SET @has_version := (
+	SELECT COUNT(*)
+	FROM information_schema.tables
+	WHERE table_schema = DATABASE()
+	  AND table_name = 'version'
+);
+SET @version_update_sql := IF(
+	@has_version > 0,
+	'UPDATE `version` SET `db_version`=''TDB 335.24081'', `cache_id`=24081 LIMIT 1',
+	'SELECT 1'
+);
+PREPARE stmt FROM @version_update_sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 UPDATE `updates` SET `state`='ARCHIVED';
